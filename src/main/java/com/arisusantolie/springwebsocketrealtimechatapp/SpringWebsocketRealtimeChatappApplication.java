1d77308eb62be4a9d3843b83ab1c4aefb19c1077
arisusantolie <arisusantolie12@gmail.com>
2021-08-29 17:33:42 +0700

    import empty spring boot project

---
+++ src/main/java/com/arisusantolie/springwebsocketrealtimechatapp/SpringWebsocketRealtimeChatappApplication.java
@@ -0,0 +1,13 @@
+package com.arisusantolie.springwebsocketrealtimechatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebsocketRealtimeChatappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebsocketRealtimeChatappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/arisusantolie/springwebsocketrealtimechatapp/SpringWebsocketRealtimeChatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arisusantolie.springwebsocketrealtimechatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebsocketRealtimeChatappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
